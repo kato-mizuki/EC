cae9829d49c6eb87df5957a260c91ecddda57c18
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-08-15 13:30:16 +0000

    Rest

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,12 @@
 class Public::HomesController < ApplicationController
   def top
-    
+    @item = Item.last(4)
   end
-  
+
   def about
-    
-  end 
-  
+
+  end
+
+ 
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.all
+    @item.count
   end
 
   def show
   end
+
 end
